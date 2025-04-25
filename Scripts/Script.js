e1dc70cc5f7b8d8abe467ae77b1a562b0aa7f5e4
Leuuo2 <leonardopereira201423@gmail.com>
2025-04-25 19:35:34 -0300

    Site-Geral 0.04

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -4,3 +4,18 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
  menuLinks.classList.toggle('active');
 });
+
+let count =1;
+    document.getElementById("radio1").checked = true;
+    setInterval(function(){
+        nextImage();
+    
+    }, 5000);
+
+function nextImage(){
+    count++;
+    if(count>4){
+        count=1;
+    }
+    document.getElementById("radio"+count).checked = true;
+}
